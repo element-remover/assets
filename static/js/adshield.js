1c6f6732245323cf9fd03da647c76953eae4d599
CrazyCodeLearner <BharatDeora.Coder@gmail.com>
2022-01-09 19:26:56 +0530

    monitor added and recreated script

--- static/js/adshield.js
+++ static/js/adshield.js
@@ -4,7 +4,7 @@ It Enable's Ad Sheild in Your Website
     <script async defer src="https://element-remover.github.io/assets/static/js/adshield.js" onload="preventClickers(allowedClicks, resetInDays)"></script>
 2. Replace the allowedClicks and resetInDays according to your need in Number
 */
-console.log("🤖 AdShield v2.6")
+console.log("🤖 AdShield v2.7")
 const $all = e => [...document.querySelectorAll(e)],
   // Cookie Getter and Setter
   cookie = (key, obj = {}) => (document.cookie.match(/[^ =]+=[^ =;]+/g).map(e => e.split("=")).map(e => obj[e[0]] = e[1]), {
@@ -22,56 +22,53 @@ const $all = e => [...document.querySelectorAll(e)],
     set: (v, e) => cookie(key).set(v, e) | local(key).set(v),
     con: v => [local(key), cookie(key)].filter(e => e != v)
   }),
+  adHider = () => $all(ads).map(e => e.getAttribute("data-shield") != true && (parnone(e), console.log(" 🥷 Hidden :", e.id))),
   parnone = el => el.setAttribute("data-shield", true) | (p = el.parentNode, [...p.children].map(e => e.tagName).filter(e => !/ins|script|iframe|style|span/i.test(e)).length == 0 && parnone(p)),
   // Wait for It
   wfi = async (f, ms = 500, count = -1) => (r = await new Promise(r => setTimeout(r, ms)), (res = f(), res ? res : count == 0 ? null : await wfi(f, ms, count - 1)));
 
-
-let style = document.createElement("style")
-style.innerHTML = `.adsHoverDiv{width:100%;height:100%;position:absolute;cursor:pointer;top:0;right:0;left:0;bottom:0;background:transparent;z-index:4;} [data-shield="true"]{display:none!important}`
+let style = document.createElement("style");
+style.innerHTML = `[data-shield]{display:none!important}`;
 document.body.appendChild(style);
 
-async function preventClickers(adClicks = 1, days = 1) {
-  try {
-    let today = new Date().setHours(0, 0, 0, 0),
-      ads = `[aria-label="Advertisement"], [id^='ezoic-ad'], [class*='ezoic-ad'], [id^='ezoic'], [id^='ezmob']`.split(",").map(e => e + ':not([data-shield])').join(","),
-      key = "hideAds",
-      clicks = "clickedAds",
-      count = parseInt(cookie(clicks).get || localStorage.getItem(clicks)) || 1,
-      value = [cookie(key).get, localStorage.getItem(key)].map(e => parseInt(e)),
-      viewed = value.indexOf(today) == -1;
+let today = new Date().setHours(0, 0, 0, 0),
+  ads = `[aria-label="Advertisement"], [id^='ezoic-ad'], [class*='ezoic-ad'], [id^='ezoic'], [id^='ezmob']`.split(",").map(e => e + ':not([data-shield])').join(","),
+  key = "hideAds",
+  value = [cookie(key).get, localStorage.getItem(key)].map(e => parseInt(e)),
+  viewed = value.indexOf(today) != -1;
 
-    console.log(" 🍪 CookieValue :", value[0], "\n 🔒 LocalValue  :", value[1], "\n ⏱️ CurrentDate :", today, "\n\n 📋 ClickAllow  :", adClicks, "\n 📸 Clicked Ads :", count);
+console.log(" 🍪 CookieValue :", value[0], "\n 🔒 LocalValue  :", value[1], "\n ⏱️ CurrentDate :", today);
 
-    // AD Protector and Hider
-    function adChecker(arr) {
-      if (viewed) {
-        arr.map(ad => ad.getAttribute("data-shield") != false && (() => {
-          let shield = document.createElement("div");
-          shield.setAttribute("class", "adsHoverDiv")
-          shield.onclick = () => {
-            console.log(" ❤️‍🩹 Shield Clicked :", ad.id, "& Clicks Left :", adClicks - count);
-            count != adClicks ? count++ : store(key).set(today, days);
-            store(clicks).set(count, days);
-            shield && shield.setAttribute("data-shield", true)
-          }
-          ad.setAttribute("data-shield", false)
-          ad.appendChild(shield);
-          console.log(" 🛡️ Created Shield :", ad.id);
-        })())
-      } else {
-        // Hide ads
-        arr.map(e => e.getAttribute("data-shield") != true && (parnone(e), console.log(" 🥷 Hidden :", e.id)));
-      }
-    }
-
-    console.log(`🔎 Finding Ads...`);
-    for (let i = 1; i < 200; i++) {
-      await wfi(() => $all(ads).length > 0, 5 * i, parseInt(10 / i)) != null && adChecker($all(ads));
-    }
-    console.log(`😍 Relax and Chill!`);
-  } catch (err) {
-    console.log(err)
+async function preventClickers() {
+  // AD Protector and Hider
+  console.log(`🔎 Finding Ads...`);
+  for (let i = 1; i < 200; i++) {
+    await wfi(() => $all(ads).length > 0, 5 * i, parseInt(10 / i)) != null && adHider();
   }
+  console.log(`😍 Relax and Chill!`);
 }
-preventClickers();
+
+if (!viewed) {
+  var monitor = setInterval(function() {
+    var elem = document.activeElement;
+    if (elem && elem.tagName == 'IFRAME') {
+      clearInterval(monitor);
+      console.log(" ❤️‍🩹 AD Clicked.");
+      store(key).set(today, days);
+      adHider($all(ads))
+    }
+  }, 100);
+} else {
+  preventClickers();
+}
+
+
+// main document must be focused in order for window blur to fire when the iframe is interacted with. 
+// There's still an issue that if user interacts outside of the page and then click iframe first without clicking page, the following logic won't run. But since the OP is only concerned about first click this shouldn't be a problem.
+// window.addEventListener("blur", () => {
+//   setTimeout(() => {
+//     if (document.activeElement.tagName === "IFRAME") {
+//        doSomething 
+//     }
+//   });
+// }, { once: true });
